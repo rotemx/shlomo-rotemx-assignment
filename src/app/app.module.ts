6f7c7f65e0d75f495918bc0d42d981aadcec6127
ShlomoRussell <93613042+ShlomoRussell@users.noreply.github.com>
2024-05-21 23:30:35 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { HeaderComponent } from './main/header/header.component';
+import { ContentComponent } from './main/content/content.component';
+import { FooterComponent } from './main/footer/footer.component';
+import { ProductListComponent } from './main/product-list/product-list.component';
+import { ShoppingCartComponent } from './main/shopping-cart/shopping-cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HeaderComponent,
+    ContentComponent,
+    FooterComponent,
+    ProductListComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule,
