a43609efcc528dd55ec4378cf3885f1a03d4f0e8
Rotem Meiri <rotemx@users.noreply.github.com>
2024-05-26 19:18:15 +0300

    rotem changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,23 @@
-import { Component }                from '@angular/core';
-import {ProductStore} from "./store/product.store";
+import {ApplicationRef, Component} from "@angular/core";
+import {ProductStore}              from "./store/product.store";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector   : "app-root",
+	templateUrl: "./app.component.html",
+	styleUrls  : ["./app.component.css"]
 })
 export class AppComponent {
-  
-  constructor(public store:ProductStore) {
-  }
-  
-  title                           = 'rotemx-assignment';
+	
+	constructor(
+		public store: ProductStore,
+		private appRef: ApplicationRef) {
+	}
+	
+	title = "rotemx-assignment";
+	routeActivated() {
+		this.appRef.tick();
+		console.log('ref ticked');
+	}
+	
 }
 
